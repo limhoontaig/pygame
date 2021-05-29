ca5bcd1acb16e8b539c0dd464ab3b42e5f5cb0b5
LimHoonTaig <limhoontaig@gmail.com>
2021-05-29 10:23:45 +0900

    initial issue

--- memory_game.py
+++ memory_game.py
@@ -1,9 +1,13 @@
 import pygame
 from random import *
-from pygame import display
 
 # set up correctly by the level
 def setup(level):
+    # How long to show the numbers to the player
+    global display_time
+    display_time = 5 - (level // 3)
+    display_time = max (display_time, 1)
+    
     # How many numbers to show to the player?
     number_count = (level // 3) +5
     number_count = min(number_count, 20) # if number_count is greater than 20, then set 20
@@ -42,45 +46,85 @@ def shuffle_grid(number_count):
 
             number_buttons.append(button)
 
-    print(grid)
-
 # view the start screen
 def display_start_screen():
     # draw a circle on the center of start button,radius 60, lineweight 5
     pygame.draw.circle(screen, WHITE, start_button.center, 60,5)
 
+    msg = game_font.render(f'{curr_level}', True, WHITE)
+    msg_rect = msg.get_rect(center=start_button.center)
+    screen.blit(msg, msg_rect)
+
+
 # display the screen of the game
 def display_game_screen():
-    # global hidden
+    global hidden
 
-    # if not hidden:
-    #     elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000 # ms -> sec
-    #     if elapsed_time > display_time:
-    #         hidden = True
+    if not hidden:
+        elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000 # ms -> sec
+        if elapsed_time > display_time:
+            hidden = True
 
     for idx, rect in enumerate(number_buttons, start=1):
-        # if hidden: # 숨김 처리
+        if hidden: # 숨김 처리
             # 버튼 사각형 그리기
-        pygame.draw.rect(screen, GRAY, rect)
-        # else:
-        #     # 실제 숫자 텍스트
-        #     cell_text = game_font.render(str(idx), True, WHITE)
-        #     text_rect = cell_text.get_rect(center=rect.center)
-        #     screen.blit(cell_text, text_rect)
+            pygame.draw.rect(screen, GRAY, rect)
+        else:
+            # 실제 숫자 텍스트
+            cell_text = game_font.render(str(idx), True, WHITE)
+            text_rect = cell_text.get_rect(center=rect.center)
+            screen.blit(cell_text, text_rect)
 
 # confirm the click position of the start button 
 def check_buttons(pos):
-    global start
-    if start_button.collidepoint(pos):
+    global start, start_ticks
+
+    if start:
+        check_number_buttons(pos)
+    elif start_button.collidepoint(pos):
         start = True
+        start_ticks = pygame.time.get_ticks() # start of the timer (stored the current time)
+
+def check_number_buttons(pos):
+    global start, hidden, curr_level
+
+    for button in number_buttons:
+        if button.collidepoint(pos):
+            if button == number_buttons[0]:
+                del number_buttons[0]
+                if not hidden:
+                    hidden = True # hide the number
+            else: # clicked the wrong number
+                game_over()
+            break
+
+    # if all numbers are correct selection, then up the level of the game to start
+    if len(number_buttons) == 0:
+        start = False
+        hidden = False
+        curr_level += 1
+        setup(curr_level)
+
+# Treated the game over, to show thw message
+def game_over():
+    global running
+    running = False
+
+    msg = game_font.render(f'Your level is {curr_level}', True,WHITE)
+    msg_rect = msg.get_rect(center=(screen_width / 2, screen_height / 2))
+
+    screen.fill(BLACK)
+    screen.blit(msg, msg_rect)
+
 
 # initialization
 pygame.init()
+pygame.font.init()
 screen_width = 1280
 screen_height = 720
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Memory Game')
-game_font = pygame.font.Font(None, 120) # define the font 
+game_font = pygame.font.Font(None, 100) # define the font 
 
 # Start button
 start_button = pygame.Rect(0,0,120,120)
@@ -92,13 +136,18 @@ WHITE = (255,255,255)
 GRAY = (50,50,50)
 
 number_buttons = [] # Buttons to push by player
+curr_level = 1 # current level
+display_time = None # the time of the showing the number
+start_ticks = None # compute the time (stored the current time)
 
 # start or not
 start = False
 
+# to show the number or not (number one clicked or over the time to show)
+hidden = False
 
-# 게임 시작전ㅇ[ 게임 설정 함수 수행
-setup(1)
+# run a init function before the game start 게임 시작전ㅇ[ 게임 설정 함수 수행
+setup(curr_level)
 
 # Game Loop
 running = True # Game is running?
@@ -111,26 +160,26 @@ while running:
             running = False # 게임이 더이상 실행중이 아님 Game is not rinning anymore
         elif event.type == pygame.MOUSEBUTTONUP: # M
             click_pos = pygame.mouse.get_pos()
-            print(click_pos)
 
     # fill the black all screen
     screen.fill(BLACK)
+
+    # display of start screen
 
     if start:
         display_game_screen() # display of the game screen
     else:
         display_start_screen() # display of the start screen
 
-    # display of start screen
-    display_start_screen()
-
-    # update of screen
-    pygame.display.update()
-
-    # is click position coordinate anywhere 
+    # if has it a clicked position (clicked anywhere)
     if click_pos:
         check_buttons(click_pos)
 
+    # update the screen
+    pygame.display.update()
+
+# to show the 5 seconds
+pygame.time.delay(5000)
 
 # Quit the game.
 pygame.quit()
