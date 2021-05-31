d0ba33584c1ed74718eb2ef74800141d20fb0be9
limhoontaig <limhoontaig@gmail.com>
2021-06-01 07:04:07 +0900

    adjust the showing location on the screen

--- Nado Game/1_create_frame.py
+++ Nado Game/1_create_frame.py
@@ -2,6 +2,6 @@ from tkinter import *
 
 root = Tk()
 root.title('Nado GUI')
-root.geometry('640x480') # width x height
+root.geometry('640x480+300+100') # width x height + x position + y Position
 
 root.mainloop()
