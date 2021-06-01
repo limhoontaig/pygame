5a8f90fae025e97f309edf0bef09770e4e281472
limhoontaig <limhoontaig@gmail.com>
2021-06-02 06:42:40 +0900

    display on the textarea for no menu

--- niceCoding/vendingmachine.py
+++ niceCoding/vendingmachine.py
@@ -10,7 +10,9 @@ def add(item):
     global sum
 
     if item not in price:
-        print('No Drink')
+        # label1['text'] ='No Drink'
+        textarea.insert(tk.INSERT, 'No drink ')
+        
 
     this_price = price.get(item)
     sum += this_price
@@ -37,7 +39,8 @@ tk.Button(frame1, text='coffee',command=lambda: add('coffee'),width=10,height=2)
 tk.Button(frame1, text='latte',command=lambda: add('latte'),width=10,height=2).grid(row=1, column=0)
 tk.Button(frame1, text='smoothie',command=lambda: add('smoothie'),width=10,height=2).grid(row=2, column=0)
 tk.Button(frame1, text='tea',command=lambda: add('tea'),width=10,height=2).grid(row=3, column=0)
-tk.Button(frame1, text='exit',command=btn_exit,width=10,height=2).grid(row=4, column=0)
+tk.Button(frame1, text='red tea',command=lambda: add('red tea'),width=10,height=2).grid(row=4, column=0)
+tk.Button(frame1, text='exit',command=btn_exit,width=10,height=2).grid(row=5, column=0)
 
 label1 = tk.Label(window, text='금액: 0원',width=100, height=2,fg='blue')
 label1.pack()
