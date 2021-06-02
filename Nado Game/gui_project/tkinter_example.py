551ead64423e9382be3084e60f96414628d9e554
limhoontaig <limhoontaig@gmail.com>
2021-06-03 06:38:17 +0900

    button completed

--- Nado Game/gui_project/tkinter_example.py
+++ Nado Game/gui_project/tkinter_example.py
@@ -6,4 +6,29 @@ root.geometry('600x480+300+100')
 
 root.resizable(False, False) 
 
+btn1 = Button(root, text='Button 111111111111')
+btn1.pack()
+
+btn2 = Button(root, padx=10, pady=10, text ='Button / save the file or not?')
+btn2.pack()
+
+btn3 = Button(root, padx=100, pady=10, text ='Button 3')
+btn3.pack()
+
+btn4 = Button(root, width=10, height=3, text ='Button / save the children')
+btn4.pack()
+
+btn5 = Button(root, padx=5, pady=3, fg='red', bg='yellow', text ='Button 5')
+
+# photo = PhotoImage(file="img.png")
+# btn6 = Button(root, image=photo,padx=5, pady=30)
+# btn6.pack()
+
+def btncmd7():
+    print('버튼이 동작하였어요.')
+
+btn7 = Button(root, text='동작하는 버튼', command = btncmd7, padx=5, pady=30)
+btn7.pack()
+
+
 root.mainloop()
