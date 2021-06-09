beda0043fb3084e3419f4333ad9df8ed70b581f0
limhoontaig <limhoontaig@gamil.com>
2021-06-09 17:47:55 +0900

    Basic window for Electrical discount computation

--- Nado Game/gui_project/Elec_discount_function.py
+++ Nado Game/gui_project/Elec_discount_function.py
@@ -4,60 +4,69 @@ from tkinter import * # __all__
 from tkinter import filedialog
 
 root = Tk()
-root.title("Nado GUI")
+root.geometry('600x300+300+150')
+root.title("전기감면 자료 작성 프로그램 Produced by LHT")
 
 # 파일 추가
-def add_file():
-    files = filedialog.askopenfilename(title="엑셀 데이타 파일을 선택하세요", \
-        filetypes=(("PNG 파일", "*.xls",'*.xlsx'), ("모든 파일", "*.*")), \
-        initialdir=r"C:\Users\Nadocoding\Desktop\PythonWorkspace\pygame_project\images")
-        # 최초에 사용자가 지정한 경로를 보여줌
+# def add_file(kind):
+#     files = filedialog.askopenfilename(title="엑셀 데이타 파일을 선택하세요", \
+#         filetypes=(("EXCEL 파일", "*.xls"), ("EXCEL 파일", "*.xlsx"), ("모든 파일", "*.*")), \
+#         initialdir=r"C:\Users\Nadocoding\Desktop\PythonWorkspace\pygame_project\images")
+#     if kind == 'welfare':
+#         txt_welfare_path.delete(0,END)
+#         txt_welfare_path.insert(0, files)
+#     else:
+#         txt_kind_welfare_path.delete(0,END)
+#         txt_kind_welfare_path.insert(0, files)
+#         # 최초에 사용자가 지정한 경로를 보여줌
     
-    # 사용자가 선택한 파일 목록
-    # for file in files:
-    #     list_file.insert(END, file)
+#     # 사용자가 선택한 파일 목록
+#     # for file in files:
+#     #     list_file.insert(END, file)
 
-# 선택 삭제
-def del_file():
-    #print(list_file.curselection())
-    for index in reversed(list_file.curselection()):
-        list_file.delete(index)
+# # 선택 삭제
+# def del_file():
+#     #print(list_file.curselection())
+#     for index in reversed(list_file.curselection()):
+#         list_file.delete(index)
 
-# 저장 경로 (폴더)
-def browse_dest_path():
-    folder_selected = filedialog.askdirectory()
-    if folder_selected is None: # 사용자가 취소를 누를 때
-        return
-    #print(folder_selected)
-    txt_dest_path.delete(0, END)
-    txt_dest_path.insert(0, folder_selected)
+# # 저장 경로 (폴더)
+# def browse_dest_path():
+#     folder_selected = filedialog.askdirectory()
+#     if folder_selected is None: # 사용자가 취소를 누를 때
+#         return
+#     #print(folder_selected)
+#     txt_dest_path.delete(0, END)
+#     txt_dest_path.insert(0, folder_selected)
 
-# 시작
-def start():
-    # 각 옵션들 값을 확인
-    print("가로넓이 : ", cmb_width.get())
-    print("간격 : ", cmb_space.get())
-    print("포맷 : ", cmb_format.get())
+# # 시작
+# def start():
+#     # 각 옵션들 값을 확인
+#     print("가로넓이 : ", cmb_width.get())
+#     print("간격 : ", cmb_space.get())
+#     print("포맷 : ", cmb_format.get())
 
-    # 파일 목록 확인
-    if file.size() == 0:
-        msgbox.showwarning("경고", "이미지 파일을 추가하세요")
-        return
+#     # 파일 목록 확인
+#     if file.size() == 0:
+#         msgbox.showwarning("경고", "이미지 파일을 추가하세요")
+#         return
 
-    # 저장 경로 확인
-    if len(txt_dest_path.get()) == 0:
-        msgbox.showwarning("경고", "저장 경로를 선택하세요")
-        return
+#     # 저장 경로 확인
+#     if len(txt_dest_path.get()) == 0:
+#         msgbox.showwarning("경고", "저장 경로를 선택하세요")
+#         return
+
+
 
 # 파일 프레임 (파일 추가, 선택 삭제)
-file_frame = Frame(root)
-file_frame.pack(fill="x", padx=5, pady=5) # 간격 띄우기
+# file_frame = Frame(root)
+# file_frame.pack(fill="x", padx=5, pady=5) # 간격 띄우기
 
-btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="파일추가", command=add_file)
-btn_add_file.pack(side="left")
+# btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="파일추가", command=add_file)
+# btn_add_file.pack(side="left")
 
-btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text="선택삭제", command=del_file)
-btn_del_file.pack(side="right")
+# btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text="선택삭제", command=del_file)
+# btn_del_file.pack(side="right")
 
 # 복지 선택 프레임
 welfare_frame = LabelFrame(root,text='한전 복지 할인 및 필수사용공제 감면자료 파일선택')
@@ -66,7 +75,7 @@ welfare_frame.pack(fill="x", padx=5, pady=5, ipady=5)
 txt_welfare_path = Entry(welfare_frame)
 txt_welfare_path.pack(side="left", fill="x", expand=True, padx=5, pady=5, ipady=4) # 높이 변경
 
-btn_welfare_path = Button(welfare_frame, text="찾아보기", width=10, command=browse_dest_path)
+btn_welfare_path = Button(welfare_frame, text="복지할인", width=10)#, command=add_file(kind='welfare'))
 btn_welfare_path.pack(side="right", padx=5, pady=5)
 
 # 복지종류 선택 프레임
@@ -76,7 +85,7 @@ kind_welfare_frame.pack(fill="x", padx=5, pady=5, ipady=5)
 txt_kind_welfare_path = Entry(kind_welfare_frame)
 txt_kind_welfare_path.pack(side="left", fill="x", expand=True, padx=5, pady=5, ipady=4) # 높이 변경
 
-btn_kind_welfare_path = Button(kind_welfare_frame, text="찾아보기", width=10, command=browse_dest_path)
+btn_kind_welfare_path = Button(kind_welfare_frame, text="할인종류", width=10)#, command=add_file(kind='kind'))
 btn_kind_welfare_path.pack(side="right", padx=5, pady=5)
 
 # 저장 경로 프레임
@@ -86,53 +95,53 @@ path_frame.pack(fill="x", padx=5, pady=5, ipady=5)
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side="left", fill="x", expand=True, padx=5, pady=5, ipady=4) # 높이 변경
 
-btn_dest_path = Button(path_frame, text="찾아보기", width=10, command=browse_dest_path)
+btn_dest_path = Button(path_frame, text="찾아보기", width=10)#, command=browse_dest_path)
 btn_dest_path.pack(side="right", padx=5, pady=5)
 
-# 옵션 프레임
-frame_option = LabelFrame(root, text="옵션")
-frame_option.pack(padx=5, pady=5, ipady=5)
+# # 옵션 프레임
+# frame_option = LabelFrame(root, text="옵션")
+# frame_option.pack(padx=5, pady=5, ipady=5)
 
-# 1. 가로 넓이 옵션
-# 가로 넓이 레이블
-lbl_width = Label(frame_option, text="가로넓이", width=8)
-lbl_width.pack(side="left", padx=5, pady=5)
+# # 1. 가로 넓이 옵션
+# # 가로 넓이 레이블
+# lbl_width = Label(frame_option, text="가로넓이", width=8)
+# lbl_width.pack(side="left", padx=5, pady=5)
 
-# 가로 넓이 콤보
-opt_width = ["원본유지", "1024", "800", "640"]
-cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
-cmb_width.current(0)
-cmb_width.pack(side="left", padx=5, pady=5)
+# # 가로 넓이 콤보
+# opt_width = ["원본유지", "1024", "800", "640"]
+# cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
+# cmb_width.current(0)
+# cmb_width.pack(side="left", padx=5, pady=5)
 
-# 2. 간격 옵션
-# 간격 옵션 레이블
-lbl_space = Label(frame_option, text="간격", width=8)
-lbl_space.pack(side="left", padx=5, pady=5)
+# # 2. 간격 옵션
+# # 간격 옵션 레이블
+# lbl_space = Label(frame_option, text="간격", width=8)
+# lbl_space.pack(side="left", padx=5, pady=5)
 
-# 간격 옵션 콤보
-opt_space = ["없음", "좁게", "보통", "넓게"]
-cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
-cmb_space.current(0)
-cmb_space.pack(side="left", padx=5, pady=5)
+# # 간격 옵션 콤보
+# opt_space = ["없음", "좁게", "보통", "넓게"]
+# cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
+# cmb_space.current(0)
+# cmb_space.pack(side="left", padx=5, pady=5)
 
-# 3. 파일 포맷 옵션
-# 파일 포맷 옵션 레이블
-lbl_format = Label(frame_option, text="포맷", width=8)
-lbl_format.pack(side="left", padx=5, pady=5)
+# # 3. 파일 포맷 옵션
+# # 파일 포맷 옵션 레이블
+# lbl_format = Label(frame_option, text="포맷", width=8)
+# lbl_format.pack(side="left", padx=5, pady=5)
 
-# 파일 포맷 옵션 콤보
-opt_format = ["PNG", "JPG", "BMP"]
-cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
-cmb_format.current(0)
-cmb_format.pack(side="left", padx=5, pady=5)
+# # 파일 포맷 옵션 콤보
+# opt_format = ["PNG", "JPG", "BMP"]
+# cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
+# cmb_format.current(0)
+# cmb_format.pack(side="left", padx=5, pady=5)
 
-# 진행 상황 Progress Bar
-frame_progress = LabelFrame(root, text="진행상황")
-frame_progress.pack(fill="x", padx=5, pady=5, ipady=5)
+# # 진행 상황 Progress Bar
+# frame_progress = LabelFrame(root, text="진행상황")
+# frame_progress.pack(fill="x", padx=5, pady=5, ipady=5)
 
-p_var = DoubleVar()
-progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
-progress_bar.pack(fill="x", padx=5, pady=5)
+# p_var = DoubleVar()
+# progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
+# progress_bar.pack(fill="x", padx=5, pady=5)
 
 # 실행 프레임
 frame_run = Frame(root)
@@ -141,8 +150,8 @@ frame_run.pack(fill="x", padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
 btn_close.pack(side="right", padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12, command=start)
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)#, command=start)
 btn_start.pack(side="right", padx=5, pady=5)
 
-root.resizable(False, False)
+root.resizable(True, True)
 root.mainloop()
