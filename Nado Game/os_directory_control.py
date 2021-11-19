f3545b1a0b11907eba6aef3ec040866d5108b240
= <=>
2021-11-19 16:00:59 +0900

    added os.walk

--- Nado Game/os_directory_control.py
+++ Nado Game/os_directory_control.py
@@ -2,10 +2,31 @@ import os
 import shutil
 
 cwd = os.getcwd()
-print(cwd)
+# print(cwd)
 rootpath = cwd+'\\pictures'
-files = os.listdir(rootpath)
-print(files)
+if not os.path.isdir(rootpath):
+  os.mkdir(rootpath)
+
+for (path, dir, files) in os.walk(rootpath):
+    for filename in files:
+        ext = os.path.splitext(filename)[-1]
+        if ext == '.jpg'|'.jpeg':
+            print("%s/%s" % (path, filename))
+
+
+
+
+
+walk = os.walk(rootpath)
+for path, direct, files in walk:
+  print(path, direct, files)
+#   total_files = []
+#   total_files.append.files
+# print(total_files)
+
+# print(walk)
+# files = os.listdir(rootpath)
+# print(files)
 for file in files:
   new_dir = file.split('_')[0]
   print(new_dir)
@@ -13,8 +34,12 @@ for file in files:
     os.mkdir(rootpath+'\\'+new_dir)
   except FileExistsError as e:
     print(e)
-  source = rootpath+'\\'+file
-  destination = rootpath+'\\'+new_dir+'\\'+file
+  source = os.path.join(rootpath,file)
+  destination = os.path.join(rootpath,new_dir,file)
   
-  shutil.move(source, destination)
+  if os.path.isdir(source):
+    pass
+  else:
+    shutil.move(source, destination)
+  
 print('end')
