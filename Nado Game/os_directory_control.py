14b4e0bd4df909adcfef1fb71806c40cc64f170f
limhoontaig <limhoontaig@gmail.com>
2021-11-22 07:03:31 +0900

    edit

--- Nado Game/os_directory_control.py
+++ Nado Game/os_directory_control.py
@@ -1,31 +1,47 @@
 import os
 import shutil
+from tkinter import filedialog, font
+
+# 저장 경로 (폴더)
+def browse_dest_path():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected is None: # 사용자가 취소를 누를 때
+      return
+    #print(folder_selected)
+    txt_dest_path.delete(0, END)
+    txt_dest_path.insert(0, folder_selected)
+
+
+folder_selected = filedialog.askdirectory()
+# if folder_selected is None: # 사용자가 취소를 누를 때
+  
+
 
 cwd = os.getcwd()
 # print(cwd)
-rootpath = cwd+'\\pictures'
+rootpath = folder_selected
 if not os.path.isdir(rootpath):
   os.mkdir(rootpath)
 
-for (path, dir, files) in os.walk(rootpath):
-    for filename in files:
-        ext = os.path.splitext(filename)[-1]
-        if ext == '.jpg'|'.jpeg':
-            print("%s/%s" % (path, filename))
+# for (path, dir, files) in os.walk(rootpath):
+#     for filename in files:
+#         ext = os.path.splitext(filename)[-1]
+#         if ext == '.jpg' or '.jpeg':
+#             print("%s/%s" % (path, filename))
 
 
 
 
 
-walk = os.walk(rootpath)
-for path, direct, files in walk:
-  print(path, direct, files)
+# walk = os.walk(rootpath)
+# for path, direct, files in walk:
+#   print(path, direct, files)
 #   total_files = []
 #   total_files.append.files
 # print(total_files)
 
 # print(walk)
-# files = os.listdir(rootpath)
+files = os.listdir(rootpath)
 # print(files)
 for file in files:
   new_dir = file.split('_')[0]
