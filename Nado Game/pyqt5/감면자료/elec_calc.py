9118a8013b12354a6e7af494c6744341f90ab6dd
= <=>
2022-05-13 14:22:41 +0900

    iii

--- Nado Game/pyqt5/감면자료/elec_calc.py
+++ Nado Game/pyqt5/감면자료/elec_calc.py
@@ -7,6 +7,11 @@ from PyQt5 import uic
 from datetime import datetime
 
 
+
+
+
+
+
 def resource_path(relative_path):
     base_path = getattr(sys, "_MAIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
