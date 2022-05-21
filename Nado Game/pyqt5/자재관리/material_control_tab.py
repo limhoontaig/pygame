5a23915694b0a4d629f6ce105cf2f66662bdf7ae
limhoontaig <limhoontaig@gmail.com>
2022-05-21 21:10:58 +0900

    under working for used material

--- Nado Game/pyqt5/자재관리/material_control_tab.py
+++ Nado Game/pyqt5/자재관리/material_control_tab.py
@@ -141,26 +141,26 @@ class ElWindow(QMainWindow, form_class):
         # 자재입고 입력 init setup
         self.dateEdit_5.setDate(QDate.currentDate())
         self.dateEdit_5.setCalendarPopup(True)
-        data = self.init_data_make()
-        for d in data[0]:
-            self.set_tbl(d)
-        self.table_display()
-
-        self.tableWidget_5.scrollToBottom
-
+        self.dateEdit_6.setDate(QDate.currentDate())
+        self.dateEdit_6.setCalendarPopup(True)
+        self.init_in_data_make()
+        self.init_out_data_make()
+        
         #items_data = data[0]
         #['독립유공 할인', '국가유공 할인', '민주유공 할인', '장애인 할인', '사회복지 할인', '기초수급 할인', '기초수급 할인 (주거/교육)', '차상위계층 할인']
         #spec_data=data[1]
         #['대가족 할인', '출산가구 할인', '다자녀 할인', '의료기기 할인']
-        self.comboBox_5.addItems(data[1])
         self.comboBox_5.activated.connect(self.comboBox_5Activated)
-        self.comboBox_6.addItems(data[2])
+        self.comboBox_9.activated.connect(self.comboBox_5Activated)
+        self.comboBox_10.activated.connect(self.out_dongho)
         self.pushButton.clicked.connect(self.addComboBoxItem)
         self.pushButton_2.clicked.connect(self.addComboBoxSpecItem)
         self.pushButton_7.clicked.connect(self.inTableToSaveExcelFile)
         self.pushButton_6.clicked.connect(self.addInMaterialToTable)
+        self.pushButton_11.clicked.connect(self.addOutMaterialToTable)
         self.lineEdit.textChanged.connect(self.lineEditChanged)
         self.lineEdit_2.textChanged.connect(self.lineEdit_2Changed)
+
         '''self.lineEdit.setText(LE[0])
         self.lineEdit_2.setText(LE[1])
         self.lineEdit_3.setText(LE[2])
@@ -177,26 +177,122 @@ class ElWindow(QMainWindow, form_class):
         self.pushButton_6.clicked.connect(self.close)
         self.pushButton_7.clicked.connect(self.my_window)
     '''
+    
+    def init_out_data_make(self):
+        df = self.out_df()
+        self.set_tbl_6(df)
+        self.out_dongho()
+        self.comboBox_8.clear()
+        self.comboBox_8.addItems(['공용','세대'])
+        data = self.comboBox_5Activated()
+        self.comboBox_9.clear()
+        self.comboBox_9.addItems(data[0])
+        self.comboBox_7.clear()
+        self.comboBox_7.addItems(data[1])
+
+        
+    
+    
+    def addOutMaterialToTable(self):
+        
+        in_data = []
+        in_data.append(self.dateEdit_6.text())
+        in_data.append(self.comboBox_10.currentText())
+        in_data.append(self.comboBox_11.currentText())
+        in_data.append(self.comboBox_8.currentText())
+        in_data.append(self.comboBox_9.currentText())
+        in_data.append(self.comboBox_7.currentText())
+        if len(self.lineEdit_6.text())==0:
+            in_data.append('**')
+        else:
+            in_data.append(self.lineEdit_6.text())
+
+        self.set_tbl_6(in_data)
+    
+    
+    
+    def set_tbl_6(self,data):
+        rowCount = self.tableWidget_6.rowCount()
+        print(rowCount)
+        rdr_col = len(data)
+        rdr_row = 1
+        #self.tableWidget_5.clear()
+        self.tableWidget_6.setRowCount(rowCount+1)
+        self.tableWidget_6.setColumnCount(len(data))
+        c = 0
+        for i in data:
+            # r = 0
+            self.tableWidget_6.setItem(rowCount, c, QTableWidgetItem(i))
+            c = c+1
+
+            #for j in i:
+            #    r = r+1
+        # self.tableWidget.setHorizontalHeaderLabels(['기존', '금월'])
+        self.table_display()
+    
+    def out_df(self):
+        file = r'C:\source\pygame\Nado Game\pyqt5\자재관리\사용대장.xlsx'
+        with pd.ExcelFile(file) as f:
+            df = pd.read_excel(f,skiprows=0)
+        return df
+
+
+    def dong_ho(self):
+        file = r'C:\source\pygame\Nado Game\pyqt5\자재관리\동호대장.xlsx'
+        with pd.ExcelFile(file) as f:
+            df = pd.read_excel(f,skiprows=0)
+            df[['동','호']] = df[['동','호']].astype('str')
+        return df
+
+    def out_dongho(self):
+        df = self.dong_ho()
+        dong = df['동'].unique()
+        self.comboBox_11.clear()
+        self.comboBox_10.addItems(dong)
+        ho = df['호'].unique()
+        df1 = df[(df['동'] == self.comboBox_10.currentText())]
+        current_ho = df1['호'].unique()
+        self.comboBox_11.clear()
+        self.comboBox_11.addItems(current_ho)
+
+    def comboBox_9Activated(self):
+        data = self.comboBox_5Activated()
+        self.comboBox_9.addItems(data[0])
+        self.comboBox_7.addItems(data[1])
+    
+    
+    
     def comboBox_5Activated(self):
         file = LE[0]#r'C:\source\pygame\Nado Game\pyqt5\자재관리\입고대장.xlsx'
         with pd.ExcelFile(file) as f:
             df = pd.read_excel(f,skiprows=0)
             df.fillna('')
         df1 = df[(df['품명'] == self.comboBox_5.currentText())]
+        items = df['품명'].unique()
         spec = df1['규격'].unique()
         self.comboBox_6.clear()
         self.comboBox_6.addItems(spec)
+        df2= df[(df['품명'] == self.comboBox_9.currentText())]
+        spec_out = df2['규격'].unique()
+        return items, spec_out
 
-    def init_data_make(self):
+    def init_in_data_make(self):
         file = LE[0]#r'C:\source\pygame\Nado Game\pyqt5\자재관리\입고대장.xlsx'
         with pd.ExcelFile(file) as f:
             df = pd.read_excel(f,skiprows=0)
-            items = df['품명'].unique()
-            spec = df['규격'].unique()
-            df[['입고수량', '구입금액','단가']] = df[['입고수량', '구입금액','단가']].astype('str')
-            df.fillna(' ')
-            list = df.values.tolist()
-        return list, items, spec    
+        items = df['품명'].unique()
+        spec = df['규격'].unique()
+        self.comboBox_5.addItems(items)
+        self.comboBox_6.addItems(spec)
+        df[['입고수량', '구입금액','단가']] = df[['입고수량', '구입금액','단가']].astype('str')
+        df.fillna(' ')
+        list = df.values.tolist()
+
+        for d in list:
+            self.set_tbl(d)
+        self.table_display()
+
+        self.tableWidget_5.scrollToBottom
 
     def inTableToSaveExcelFile(self):
         file = LE[0]#r'C:\source\pygame\Nado Game\pyqt5\자재관리\입고대장.xlsx'
