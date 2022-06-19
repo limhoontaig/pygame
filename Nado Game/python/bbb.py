71c39605c2211f3c0e2f4fa84ccb2231c41fbfab
limhoontaig <limhoontaig@gmail.com>
2022-06-20 07:32:54 +0900

    interim

---
+++ Nado Game/python/bbb.py
@@ -0,0 +1,15 @@
+class BBB:
+    def setup():
+        print("setup!")
+
+    def teardown():
+        print("teardown!")
+
+    def testModule1():
+        print("testModule1!")
+
+    def testModule2():
+        print("testModule2!")
+
+    def testModule3():
+        print("testModule3!")
