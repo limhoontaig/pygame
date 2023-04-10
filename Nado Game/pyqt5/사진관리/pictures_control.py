b2bb9b073f77626213bf9f536f436871042f06da
limhoontaig <limhoontaig@gmail.com>
2023-04-11 06:54:09 +0900

    mp4 to label_8 Show completion

--- Nado Game/pyqt5/사진관리/pictures_control.py
+++ Nado Game/pyqt5/사진관리/pictures_control.py
@@ -662,6 +662,45 @@ class ElWindow(QMainWindow, form_class):
                     pixmap = pixmap.scaledToHeight(height)
                 self.label_8.setPixmap(QPixmap(pixmap))
                 self.label_8.adjustSize()
+
+    def array_to_qimage(self, arr, copy=False):
+        """
+        Convert NumPy array to QImage object
+        
+        :param numpy.array arr: NumPy array
+        :param bool copy: if True, make a copy of the array
+        :return: QImage object
+        """
+        # https://gist.githubusercontent.com/smex/5287589/raw/toQImage.py
+
+        if arr is None:
+            return QImage()
+        if len(arr.shape) not in (2, 3):
+            raise NotImplementedError("Unsupported array shape %r" % arr.shape)
+        data = arr.data
+        ny, nx = arr.shape[:2]
+        stride = arr.strides[0]  # bytes per line
+        color_dim = None
+        if len(arr.shape) == 3:
+            color_dim = arr.shape[2]
+        if arr.dtype == np.uint8:
+            if color_dim is None:
+                qimage = QImage(data, nx, ny, stride, QImage.Format_Indexed8)
+    #            qimage.setColorTable([qRgb(i, i, i) for i in range(256)])
+                qimage.setColorCount(256)
+            elif color_dim == 3:
+                qimage = QImage(data, nx, ny, stride, QImage.Format_RGB888)
+            elif color_dim == 4:
+                qimage = QImage(data, nx, ny, stride, QImage.Format_ARGB32)
+            else:
+                raise TypeError("Invalid third axis dimension (%r)" % color_dim)
+        elif arr.dtype == np.uint32:
+            qimage = QImage(data, nx, ny, stride, QImage.Format_ARGB32)
+        else:
+            raise NotImplementedError("Unsupported array data type %r" % arr.dtype)
+        if copy:
+            return qimage.copy()
+        return qimage
             
     def qMediaViewer(self, img):
         #if img:
@@ -669,14 +708,17 @@ class ElWindow(QMainWindow, form_class):
         if img == 'Null':
             QMessageBox.information(self, "Image Viewer", "Cannot Load %s." % img)
             return
-        #h, w, d = img.shape
-        #qimage = QImage(img.data, w, h, d*w, QImage.Format_RGB888)
         
-        qimage = qimage2ndarray.array2qimage(img, normalize=False)
+        #qimage = qimage2ndarray.array2qimage(img)#, normalize=True)#False)
 
-
-
-
+        #qimage = self.array_to_qimage(img, copy=False)
+        if len(img.shape)<3:
+            frame = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        else:
+            frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        h, w, d = img.shape
+        qimage = QImage(frame.data, w, h, d*w, QImage.Format_RGB888)
 
         pixmap = QPixmap(qimage)
         self.label_8.setPixmap(QPixmap(pixmap))
